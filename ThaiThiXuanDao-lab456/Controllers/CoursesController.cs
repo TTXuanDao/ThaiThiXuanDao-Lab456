a65ac2279d1d9c279b65639aa56c9942c89622e5
Administrator <Administrator@PC254>
2020-06-04 11:05:18 +0700

    Add project files.

---
+++ ThaiThiXuanDao-lab456/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThaiThiXuanDao_lab456.Models;
+using ThaiThiXuanDao_lab456.ViewModels;
+
+namespace ThaiThiXuanDao_lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+            
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+       
+    }
+}
