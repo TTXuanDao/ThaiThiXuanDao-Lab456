117e03d57dc90fba72f541a602e81d59bad553ed
Administrator <Administrator@PC257>
2020-06-11 11:09:28 +0700

    Ngay 2 commit

--- ThaiThiXuanDao-lab456/Controllers/CoursesController.cs
+++ ThaiThiXuanDao-lab456/Controllers/CoursesController.cs
@@ -30,8 +30,14 @@ namespace ThaiThiXuanDao_lab456.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
             var course = new Course
             {
                 LecttureId = User.Identity.GetUserId(),
--- ThaiThiXuanDao-lab456/ViewModels/CourseViewModel.cs
+++ ThaiThiXuanDao-lab456/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using ThaiThiXuanDao_lab456.Models;
@@ -8,9 +9,16 @@ namespace ThaiThiXuanDao_lab456.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
